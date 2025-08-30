65413c36820baed1c83146c5a7d200a75d6c355e
Dinesh <itsdineshshinde@gmail.com>
2025-08-30 23:01:30 +0530

    Create jsondecode.tf and json file

---
+++ jsondecode.tf
@@ -0,0 +1,8 @@
+locals {
+    contents = file("test-json.json") #fetch the file present in the local workspace.
+    decode = jsondecode(local.contents)
+}
+
+output "jsondecode" {
+    value = local.decode["name"] 
+}
