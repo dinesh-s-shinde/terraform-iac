f86e6754f7b4ddf1926d4219d8b7a67a35fdf93d
Dinesh <itsdineshshinde@gmail.com>
2025-09-06 16:08:53 +0530

    Create file

---
+++ jsonencode.tf
@@ -0,0 +1,12 @@
+resource "local_file" "demofile" {
+    filename = "dev.json"
+    content = jsonencode({
+        name = "Diya"
+        age = 01
+        city = "Bengaluru"
+    })
+}
+
+output "jsonencode_file" {
+    value = local_file.demofile.content
+}
